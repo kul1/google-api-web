b3a230f86b8ea58d048b654dbbca372c2e4bb538
P Kul <1.0@kul.asia>
2018-04-16 23:12:32 -0500

    Test @acess_token

--- spec/web_client_secret_spec.rb
+++ spec/web_client_secret_spec.rb
@@ -2,4 +2,21 @@ require 'google/api_client/client_secrets'
 
 describe Google::APIClient::ClientSecrets do
 
+	describe '::new' do
+		let(:client_id) do
+			Google::Auth::ClientId.new(ENV['GOOGLE_CLIENT_ID'],ENV['GOOGLE_CLIENT_SECRET']).client
+			Google::Auth::Stores::RedisTokenStore.new(redis: Redis.new)
+		end
+		
+		context 'without option' do
+			subject { google::APIClient::ClientSecrets.new(data) }
+
+			describe 'existance of instannce variables' do
+				it '@access_token' do
+					expect(subject).to respond_to(:access_token)
+				end
+			end
+		end
+
+	end
 end
